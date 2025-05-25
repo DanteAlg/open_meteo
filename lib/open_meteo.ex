3e3f3ccda55b6146d0058e7f376191d478d9a51a
Dante Miranda <dante.alighierimds@gmail.com>
2025-05-25 16:13:01 -0300

    Add paralel process to get avarage temperature to cities

--- lib/open_meteo.ex
+++ lib/open_meteo.ex
@@ -1,2 +1,15 @@
 defmodule OpenMeteo do
+  alias OpenMeteo.Insights
+
+  def call do
+    spawn_monitor(__MODULE__, :average_temperature, ["São Paulo"])
+    spawn_monitor(__MODULE__, :average_temperature, ["Belo Horizonte"])
+    spawn_monitor(__MODULE__, :average_temperature, ["Curitiba"])
+  end
+
+  @spec average_temperature(binary()) :: :ok
+  def average_temperature(city) do
+    temp = Insights.avarage_max_temperature(city)
+    IO.puts("#{city}: #{temp}C°")
+  end
 end
--- test/open_meteo_test.exs
+++ test/open_meteo_test.exs
@@ -1,8 +1,3 @@
 defmodule OpenMeteoTest do
-  use ExUnit.Case
-  doctest OpenMeteo
-
-  test "greets the world" do
-    assert OpenMeteo.hello() == :world
-  end
+  use OpenMeteo.ConnCase
 end
